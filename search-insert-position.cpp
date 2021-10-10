3246a163dc9aaedc76918ebfd88a28b0e1cadd1d
Tony <kavinjsir@gmail.com>
2021-10-10 10:07:28 -0400

    search--insert-position: 0ms, 9.7MB

--- search-insert-position.cpp
+++ search-insert-position.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
   int searchInsert(vector<int>& nums, int target) {
-    int i;
-    int len = nums.size();
-    for (i = 0; i < len; i++) {
-      if (nums[i] >= target) {
-        break;
-      }
+    int l = 0;
+    int r = nums.size() - 1;
+    int m;
+    
+    while (l <= r ) {
+      m = (l + r) / 2;
+      if (nums[m] == target) return m;
+      if (nums[m] < target) l = m + 1;
+      else r = m - 1;
     }
-    return i;
+    if (nums[m] < target) return m + 1;
+    return m;
   }
-}; 
+};
