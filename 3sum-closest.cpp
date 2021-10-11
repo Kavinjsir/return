5e7280628110efe79ed7fea492b582fc58d13a57
Tony <kavinjsir@gmail.com>
2021-10-10 23:05:18 -0400

    3sum: 4ms, 10MB

--- 3sum-closest.cpp
+++ 3sum-closest.cpp
@@ -1,18 +1,22 @@
-#include <bits/stdc++.h>
-#include <cmath>
-
 class Solution {
 public:
   int threeSumClosest(vector<int>& nums, int target) {
-    int i, j, k, tmp = INT_MAX - abs(target), len = nums.size(), sum;
-    for (i = 0; i < len - 2; i++) {
-      for (j = i + 1; j < len - 1; j++) {
-        for (k = j + 1; k < len; k++) {
-          sum = nums[i] + nums[j] + nums[k];
-          if (sum == target) return target;
-          if (abs(target - sum)  < abs(target - tmp)) {
-            tmp = sum;
-          }
+    sort(nums.begin(), nums.end());
+    int tmp = INT_MAX - abs(target);
+    
+    for (int i = 0; i < nums.size() - 2; i++) {
+      int j = i + 1;
+      int k = nums.size() - 1;
+      
+      while(j < k) {
+        int sum = nums[i] + nums[j] + nums[k];
+        if (sum == target) return target;
+        
+        if (abs(target - sum) < abs(target - tmp)) { tmp = sum; }
+        if (sum < target) {
+          j++;
+        } else {
+          k--;
         }
       }
     }
