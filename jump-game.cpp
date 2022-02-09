40ae12128d33f361bd9654e00710210f76c3fa0f
Tony <kavinjsir@gmail.com>
2022-02-08 20:19:22 -0500

    jump-game: 71ms, 48.5MB

--- jump-game.cpp
+++ jump-game.cpp
@@ -6,18 +6,24 @@ public:
       
       m[0] = true;
       
+      int maxTrueIndex = 0;
+
       for (int i = 0; i < len; i++) {
         if (m[i] == false) continue;
-        
+
         int steps = nums[i];
+        if (i + steps <= maxTrueIndex) continue;
+
+        maxTrueIndex = i + steps;
+
+        if (maxTrueIndex >= len - 1) return true;
+
         for (int j = 1; j <= steps; j++) {
           if (i + j >= len) break;
-          
+
           m[i + j] = true;
         }
-
       }
-      
       return m[len - 1];
     }
 };
