cd69be60ff1550cb021de776c6572a9b922e0c45
Tony <kavinjsir@gmail.com>
2022-02-08 21:05:56 -0500

    jump-gameII: 12ms, 16.5MB

--- jump-gameII.cpp
+++ jump-gameII.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
   int jump(vector<int>& nums) {
-    int len = nums.size();
-    vector<int> m(len, INT_MAX - 1);
-    m[0] = 0;
-    
-    for (int i = 0; i < len; i++) {
-      int range = nums[i];
-      
-      for (int j = 1; j <= range; j++) {
-        if (i + j >= len) break;
-        
-        m[i + j] = min(m[i + j], m[i] + 1);
+    int i = 0, len = nums.size(), steps = 0, reach = 0, maxReach = 0;
+    while(reach < len - 1) {
+      steps++;
+      for (; i <= reach; i++) {
+        maxReach = max(maxReach, i + nums[i]);
+        if (maxReach >= len - 1) return steps;
       }
+      reach = maxReach;
     }
-    
-    return m[len - 1];
+    return steps;
   }
 };
 
