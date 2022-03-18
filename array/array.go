00f076f43d96ca8236ecb696318bc8afa016673b
Tony <kavinjsir@gmail.com>
2022-03-17 20:07:48 -0400

    feat: Implement array insertion

--- array/array.go
+++ array/array.go
@@ -1,6 +1,9 @@
 package array
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type MyArray struct {
 	data []int
@@ -28,6 +31,33 @@ func SetCapcity(capacity int) SetArrayOption {
 	return func(arr *MyArray) {
 		arr.data = make([]int, capacity)
 	}
+}
+
+func (arr *MyArray) Insert(element int, index int) error {
+	if arr.size == cap(arr.data) {
+		return errors.New("Exceed Capacity Limit.")
+	}
+
+	if index > arr.size || index < 0 {
+		return errors.New("Index outof Range.")
+	}
+
+	for pos := arr.size; pos > index; pos-- {
+		arr.data[pos] = arr.data[pos-1]
+	}
+
+	arr.data[index] = element
+	arr.size = arr.size + 1
+
+	return nil
+}
+
+func (arr *MyArray) AddFirst(element int) error {
+	return arr.Insert(0, element)
+}
+
+func (arr *MyArray) AddLast(element int) error {
+	return arr.Insert(arr.size, element)
 }
 
 func (arr *MyArray) GetCapacity() int {
