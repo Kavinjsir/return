c732df88c36f155ca37a7a47bad7ca81beeb21cf
Tony <kavinjsir@gmail.com>
2022-03-19 18:26:10 -0400

    leetcode: minimum-size-subarray-sum: 3ms, 3.8MB

--- leetcode/array/minimum-size-subarray-sum.go
+++ leetcode/array/minimum-size-subarray-sum.go
@@ -1,9 +1,9 @@
 // https://leetcode.com/problems/minimum-size-subarray-sum
 func minSubArrayLen(target int, nums []int) int {
-	l, r := 0, -1
-	sum := 0
+	l, r, sum := 0, -1, 0
 	size := len(nums)
 	res := size + 1
+
 	for l < size {
 		if r+1 < size && sum < target {
 			r++
@@ -16,10 +16,12 @@ func minSubArrayLen(target int, nums []int) int {
 		if sum >= target && r-l+1 < res {
 			res = r - l + 1
 		}
+
 	}
 
 	if res > size {
 		return 0
 	}
+
 	return res
 }
