4e10686fa1c58574b6772008408b64aafd30eb8a
Tony <kavinjsir@gmail.com>
2022-04-03 01:53:44 -0400

    leetcode: valid-palindrome-ii: 70ms, 24.1MB

--- leetcode/valid-palindrome-ii.cpp
+++ leetcode/valid-palindrome-ii.cpp
@@ -4,18 +4,17 @@ public:
     int i = 0;
     int j = s.size() - 1;
 
-    while (i <= j) {
-      if (s[i] == s[j]) {
-        i++;
-        j--;
-      } else
+    while (i < j) {
+      if (s[i] != s[j])
         return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+      i++;
+      j--;
     }
     return true;
   }
 
   bool isPalindrome(string s, int i, int j) {
-    while (i <= j) {
+    while (i < j) {
       if (s[i] != s[j])
         return false;
       i++;
