1a7d0969a37ab741d282bfc1482fe8dc4e47b5b8
Tony <kavinjsir@gmail.com>
2022-04-06 18:45:20 -0400

    leetcode: merge-sorted-array: 0ms, 2.2MB

--- leetcode/array/merge-sorted-array.go
+++ leetcode/array/merge-sorted-array.go
@@ -1,50 +1,25 @@
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	p1, p2, i := m+n-1, m-1, 0
-
-	if n == 0 {
-		return
-	}
-
-	if m == 0 {
-		copy(nums1, nums2)
-		return
-	}
-
-	for p2 >= 0 {
-		nums1[p1] = nums1[p2]
-		p1--
-		p2--
-	}
-
-	p1 = n
-	p2 = 0
-
-	for p1 < m+n && p2 < n {
-		if nums1[p1] < nums2[p2] {
-			nums1[i] = nums1[p1]
-			p1++
+	i := m + n - 1
+	m--
+	n--
+	for m >= 0 && n >= 0 {
+		if nums1[m] > nums2[n] {
+			nums1[i] = nums1[m]
+			m--
 		} else {
-			nums1[i] = nums2[p2]
-			p2++
+			nums1[i] = nums2[n]
+			n--
 		}
-		i++
+		i--
 	}
-
-	if p2 >= n {
-		for p1 < m+n {
-			nums1[i] = nums1[p1]
-			i++
-			p1++
-		}
-		return
+	for m >= 0 {
+		nums1[i] = nums1[m]
+		m--
+		i--
 	}
-
-	if p1 >= m+n {
-		for p2 < n {
-			nums1[i] = nums2[p2]
-			i++
-			p2++
-		}
-		return
+	for n >= 0 {
+		nums1[i] = nums2[n]
+		n--
+		i--
 	}
 }
