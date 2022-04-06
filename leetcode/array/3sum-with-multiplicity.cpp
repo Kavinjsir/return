4aed67071d223324f84632bf7878e57e7566b31a
Tony <kavinjsir@gmail.com>
2022-04-06 17:42:46 -0400

    leetcode: 3sum-with-multiplicity: 6ms, 10.4MB

--- leetcode/array/3sum-with-multiplicity.cpp
+++ leetcode/array/3sum-with-multiplicity.cpp
@@ -1,61 +1,29 @@
 class Solution {
 public:
-  int threeSumMulti(vector<int> &arr, int target) {
-    sort(arr.begin(), arr.end());
-    int l = arr.size();
+  int threeSumMulti(vector<int> &arr, int t) {
+    int freq[101] = {0}, MAX = 0;
+    long long ans = 0, mod = 1000000007;
 
-    long long kMod = 1e9 + 7;
-    long long count = 0;
+    for (auto &num : arr) {
+      freq[num]++;
+      MAX = max(MAX, num);
+    }
 
-    for (int i = 0; i < l - 2; i++) {
-      int rest = target - arr[i], j = i + 1, k = l - 1;
-      while (j < k) {
-        if (arr[j] + arr[k] == rest) {
-          if (arr[j] == arr[k]) {
-            count += ((k - j + 1) * (k - j) / 2);
-            break;
-          }
-          int left = 1, right = 1;
-          while (j + 1 < k && arr[j] == arr[j + 1]) {
-            left++;
-            j++;
-          }
-          while (k - 1 > j && arr[k] == arr[k - 1]) {
-            right++;
-            k--;
-          }
-          count += (left * right);
-          j++;
-          k--;
-        } else if (arr[j] + arr[k] < rest) {
-          j++;
-        } else {
-          k--;
-        }
+    for (int i = 0; i <= MAX; i++) {
+      if (i + i + i == t)
+        ans += (((long)freq[i] * (freq[i] - 1) * (freq[i] - 2)) / 6);
+      for (int j = i + 1; j <= MAX; j++) {
+        if (i + i + j == t)
+          ans += (freq[i] * (freq[i] - 1) >> 1) * freq[j];
+        else if (i + j + j == t)
+          ans += (freq[j] * (freq[j] - 1) >> 1) * freq[i];
+        else
+          ans += (t - i - j > j && t - i - j <= MAX
+                      ? freq[i] * freq[j] * freq[t - i - j]
+                      : 0);
       }
     }
 
-    return (count % kMod);
+    return ans % mod;
   }
 };
-
-/*
-class Solution {
-public:
-  int threeSumMulti(vector<int>& arr, int target) {
-    long long kMod = 1e9 + 7, count = 0;
-    unordered_map<int, int> m;
-
-    int l = arr.size();
-    for (int k = 1; k < l; k++) {
-      count += m[target - arr[k]];
-
-      for (int j = 0; j < k; j++) {
-        m[arr[j] + arr[k]]++;
-      }
-    }
-
-    return count % kMod;
-  }
-};
-*/
