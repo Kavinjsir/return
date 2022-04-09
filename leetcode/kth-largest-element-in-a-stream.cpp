b1994b73cffca1b525cb0781c64b767dbeffcb8e
Tony <kavinjsir@gmail.com>
2022-04-08 20:41:15 -0400

    leetcode: kth-largest-element-in-a-stream: 43ms, 20MB

---
+++ leetcode/kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,33 @@
+class KthLargest {
+public:
+  vector<int> before;
+  vector<int> after;
+  int size;
+  KthLargest(int k, vector<int> &nums) {
+    sort(nums.begin(), nums.end(), greater<int>());
+    int i = 0, l = nums.size();
+    while (i < k && i < l) {
+      before.push_back(nums[i]);
+      i++;
+    }
+    while (i < l) {
+      after.push_back(nums[i]);
+      i++;
+    }
+    make_heap(before.begin(), before.end(), greater<int>{});
+    size = k;
+  }
+
+  int add(int val) {
+    if (before.size() < size) {
+      before.push_back(val);
+      make_heap(before.begin(), before.end(), greater<int>{});
+    } else if (val > before.front()) {
+      before.push_back(val);
+      pop_heap(before.begin(), before.end(), greater<int>{});
+      before.pop_back();
+    }
+
+    return before.front();
+  }
+};
