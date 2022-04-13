45697b4ef6c2c30630364087ad55d4c48e218ebe
Tony <kavinjsir@gmail.com>
2022-04-13 18:33:22 -0400

    leetcode: spiral-matrix-ii: 0ms, 6.5MB

--- leetcode/array/spiral-matrix-ii.cpp
+++ leetcode/array/spiral-matrix-ii.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
   vector<vector<int>> generateMatrix(int n) {
-    int upBd = -1, rtBd = n, dwBd = n, ltBd = -1, k = 1;
+    int upBd = -1, rtBd = n, dwBd = n, ltBd = -1, k = 0;
     int i = ltBd + 1, j = upBd + 1;
     vector<vector<int>> r(n, vector<int>(n));
 
-    while (k <= n * n) {
+    while (k < n * n) {
       for (j = ltBd + 1; j < rtBd; j++) {
-        r[i][j] = k;
-        k++;
+        r[i][j] = (++k);
       }
       upBd++;
       j--;
 
       for (i = upBd + 1; i < dwBd; i++) {
-        r[i][j] = k;
-        k++;
+        r[i][j] = (++k);
       }
       rtBd--;
       i--;
 
       for (j = rtBd - 1; j > ltBd; j--) {
-        r[i][j] = k;
-        k++;
+        r[i][j] = (++k);
       }
       dwBd--;
       j++;
 
       for (i = dwBd - 1; i > upBd; i--) {
-        r[i][j] = k;
-        k++;
+        r[i][j] = (++k);
       }
       ltBd++;
       i++;
