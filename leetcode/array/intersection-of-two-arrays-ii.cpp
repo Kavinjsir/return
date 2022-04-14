423a8bb675b27eac13860e733037bdaf167858d3
Tony <kavinjsir@gmail.com>
2022-04-14 14:42:19 -0400

    leetcode: intersection-of-two-arrays-ii: 0ms, 9.9MB

--- leetcode/array/intersection-of-two-arrays-ii.cpp
+++ leetcode/array/intersection-of-two-arrays-ii.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
   vector<int> intersect(vector<int> &nums1, vector<int> &nums2) {
-    unordered_map<int, int> r;
+    int r[1001] = {0};
     for (auto c : nums1)
       r[c]++;
 
     vector<int> s;
     for (auto c : nums2) {
-      if (r[c] && r[c] > 0) {
+      if (r[c] > 0) {
         s.push_back(c);
         r[c]--;
       }
