ce24250d9d850043e22668eeb69c6ddb3297fc97
Tony <kavinjsir@gmail.com>
2022-04-15 10:13:58 -0400

    leetcode: reshape-the-matrix: 10ms, 10.7MB Improve simplicity with math skills

--- leetcode/array/reshape-the-matrix.cpp
+++ leetcode/array/reshape-the-matrix.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
   vector<vector<int>> matrixReshape(vector<vector<int>> &mat, int r, int c) {
-    int m = mat.size(), n = mat[0].size(), p = 0, q = 0;
-    if (int(m * n) != int(r * c))
+    int m = size(mat), n = size(mat[0]), total = m * n;
+    if (r * c != total)
       return mat;
+
     vector<vector<int>> s(r, vector<int>(c));
-    for (int i = 0; i < r; i++) {
-      for (int j = 0; j < c; j++) {
-        s[i][j] = mat[p][q];
-        if (q + 1 == n) {
-          p++;
-          q = 0;
-        } else {
-          q++;
-        }
-      }
+
+    for (int i = 0; i < total; i++) {
+      s[i / c][i % c] = mat[i / n][i % n];
     }
+
     return s;
   }
 };
