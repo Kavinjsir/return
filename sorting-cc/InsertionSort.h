a43bb70efead53e86389fdea51f81e3aae9f5cc0
Tony <kavinjsir@gmail.com>
2022-04-25 12:31:13 -0400

    chore: Remove unnecessary code

--- sorting-cc/InsertionSort.h
+++ sorting-cc/InsertionSort.h
@@ -1,18 +1,6 @@
 #ifndef INSERTIONSORT_H
 #define INSERTIONSORT_H
 
-template <typename T> void insertionSort(T arr[], int n) {
-  for (int i = 1; i < n; i++) {
-    T e = arr[i];
-    int j;
-    for (j = i; j > 0 && arr[j - 1] > e; j--) {
-      arr[j] = arr[j - 1];
-    }
-    arr[j] = e;
-  }
-  return;
-}
-
 // Insertion sort from [l..r] for array "arr"
 template <typename T> void insertionSort(T arr[], int l, int r) {
   for (int i = l + 1; i <= r; i++) {
