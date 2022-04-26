bfe91615f5398e0a59fd668ae6077c0f915fcf4d
Tony <kavinjsir@gmail.com>
2022-04-26 14:13:17 -0400

    leetcode: rotate-array: 32ms, 8.9MB

--- leetcode/array/rotate-array.go
+++ leetcode/array/rotate-array.go
@@ -1,14 +1,17 @@
+// https://github.com/golang/go/wiki/SliceTricks#reversing
+
+func reverse(a []int) {
+	l := len(a)
+	for i := l/2 - 1; i >= 0; i-- {
+		opp := l - 1 - i
+		a[i], a[opp] = a[opp], a[i]
+	}
+}
+
 func rotate(nums []int, k int) {
 	k = k % len(nums)
-	sort.SliceStable(nums, func(i, j int) bool {
-		return true
-	})
-
-	sort.SliceStable(nums[:k], func(i, j int) bool {
-		return true
-	})
-
-	sort.SliceStable(nums[k:], func(i, j int) bool {
-		return true
-	})
+	reverse(nums)
+	reverse(nums[:k])
+	reverse(nums[k:])
 }
+
