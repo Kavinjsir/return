67310badf954aaaf5b3443ee5d8d433a2e6787e4
Tony <kavinjsir@gmail.com>
2022-04-28 12:29:01 -0400

    leetcode: reverse-words-in-a-string-iii: 7ms, 9.6MB

--- leetcode/reverse-words-in-a-string-iii.cpp
+++ leetcode/reverse-words-in-a-string-iii.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
-  void reverse(string &str) {
-    int n = str.length(), i = 0;
-    while (i < n / 2) {
-      swap(str[i], str[n - i - 1]);
-      i++;
-    }
-  }
-
   string reverseWords(string s) {
-    istringstream iss(s);
-    vector<string> tokens{istream_iterator<string>{iss},
-                          istream_iterator<string>{}};
-    ostringstream o;
-    auto it = tokens.begin();
-    if (it != tokens.end()) {
-      reverse(*it);
-      o << *it++;
-      for (; it != tokens.end(); ++it) {
-        reverse(*it);
-        o << " " << *it;
+    int i = 0, l = s.size();
+    auto begin = s.begin();
+    for (int j = 0; j < l; ++j) {
+      if (s[j] == ' ') {
+        reverse(begin + i, begin + j);
+        i = j + 1;
       }
     }
-    return o.str();
+    reverse(begin + i, s.end());
+    return s;
   }
 };
